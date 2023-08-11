b3d438966c91a9601b3ee53d6884f8f17807f70b
seogwang <seogwang7779@gmail.com>
2023-08-11 13:02:13 +0900

    masscom fungtional work

---
+++ week01/src/com/week/dto/Masscom.java
@@ -0,0 +1,40 @@
+package com.week.dto;
+
+public class Masscom {
+    private int mno;
+    private String title;
+    private String link;
+    private String resdate;
+
+    public int getMno() {
+        return mno;
+    }
+
+    public void setMno(int mno) {
+        this.mno = mno;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getResdate() {
+        return resdate;
+    }
+
+    public void setResdate(String resdate) {
+        this.resdate = resdate;
+    }
+}
